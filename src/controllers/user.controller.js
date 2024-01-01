843040bb7040afe56e9985c3ee1d84514ff14c1d
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2024-01-02 00:14:57 +0530

    added refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from 'jsonwebtoken'
 
 
 const generateAccessAndRefreshTokens = async (userId) =>
@@ -105,15 +106,20 @@ const registerUser = asyncHandler(async (req, res) =>
 const loginUser = asyncHandler(async (req, res) =>
 {
     const { email, username, password } = req.body;
-    if (!username || !email)
+    if (!username && !email)
     {
         throw new ApiError(400, "Username or Email field cannot be empty!");
     }
 
+    // if (!(username || email))
+    // {
+    //     throw new ApiError(400, "Username or Email field cannot be empty!");
+    // }
+
     const userdata = await User.findOne({
         $or: [
             { 'email': email },
-            { 'username': username.toLowerCase() }
+            { 'username': username?.toLowerCase() }
         ]
     });
 
@@ -131,7 +137,7 @@ const loginUser = asyncHandler(async (req, res) =>
 
     const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(userdata._id);
 
-    const loggedInUser = User.findById(userdata._id).select("-password -refreshToken");
+    const loggedInUser = await User.findById(userdata._id).select("-password -refreshToken");
 
     const options = {
         httpOnly: true,
@@ -183,4 +189,59 @@ const logoutUser = asyncHandler(async (req, res) =>
 });
 
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res) =>
+{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken)
+    {
+        throw new ApiError(401, "unauthorized request")
+    }
+
+    try
+    {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+
+        const user = await User.findById(decodedToken?._id)
+
+        if (!user)
+        {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken)
+        {
+            throw new ApiError(401, "Refresh token is expired or used")
+
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+
+        const { accessToken, newRefreshToken } = await generateAccessAndRefereshTokens(user._id)
+
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", newRefreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    { accessToken, refreshToken: newRefreshToken },
+                    "Access token refreshed"
+                )
+            )
+    } catch (error)
+    {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+
+})
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,32 +60,31 @@ userSchema.methods.isPasswordCorrect = async function (password)
 
 userSchema.methods.generateAccessToken = function ()
 {
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
-            username: this.username,
             email: this.email,
+            username: this.username,
             fullName: this.fullName
-
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-    );
+    )
 }
-
 userSchema.methods.generateRefreshToken = function ()
 {
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
+
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-    );
+    )
 }
 
 
