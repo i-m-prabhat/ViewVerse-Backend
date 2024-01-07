03ab5c3f3990ce315da8026b17eda4cf301f87c3
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2024-01-07 21:39:32 +0530

    assignments update

--- src/app.js
+++ src/app.js
@@ -13,7 +13,14 @@ app.use(cookieParser());
 
 //routes import 
 import userRouter from './routes/user.routes.js'
-
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 app.get("/", (req, res) =>
@@ -24,7 +31,15 @@ app.get("/", (req, res) =>
     });
 });
 
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export { app };
